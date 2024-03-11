5f008b6ab255130babcbe16ec4ed0ad111f9a0b3
Youssef Mohamed / Yusuf Kaya <38719545+yousefabohend@users.noreply.github.com>
2024-03-11 17:54:53 +0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,12 @@ fn main() {
 
     if command == "decode" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
+        //println!("Logs from your program will appear here!");
 
         // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
