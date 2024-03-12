cd861a798ad07df789d7788f2b6aa500df5a4dd3
Youssef Mohamed / Yusuf Kaya <38719545+yousefabohend@users.noreply.github.com>
2024-03-12 04:25:51 +0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
+
 use serde_json;
 use std::env;
 
 // Available if you need it!
-// use serde_bencode
+use serde_bencode;
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> anyhow::Result<serde_json::Value> {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    }else if encoded_value.chars().next().unwrap() == 'i' {
-        // Example: "i52e" -> "52"
-        let e_index = encoded_value.find('e').unwrap();
-        let number_string = &encoded_value[1..e_index];
-        let number = number_string.parse::<i64>().unwrap();
-        return serde_json::Value::Number(serde_json::Number::from(number));
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    let value: serde_bencode::value::Value = serde_bencode::from_str(encoded_value)?;
+    convert(value)
+}
+fn convert(value: serde_bencode::value::Value) -> anyhow::Result<serde_json::Value> {
+    match value {
+        serde_bencode::value::Value::Bytes(b) => 
+            Ok(serde_json::Value::String(String::from_utf8(b)?)),
+        serde_bencode::value::Value::Int(i) => Ok(serde_json::Value::Number(   serde_json::Number::from(i))),
+        serde_bencode::value::Value::List(l) => {
+            let mut vec = Vec::new();
+            for v in l {
+                vec.push(convert(v)?);
+            }
+            Ok(serde_json::Value::Array(vec))
+        }
+        serde_bencode::value::Value::Dict(d) => {
+            let mut map = serde_json::Map::new();
+            for (k, v) in d {
+                map.insert(String::from_utf8(k)?, convert(v)?);
+            }
+            Ok(serde_json::Value::Object(map))
+        }
+        /*_ => {
+            panic!("Unreachable encoded value{:?}", value)
+        }*/
     }
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main() {
+fn main() -> anyhow::Result<()>{
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
@@ -36,9 +47,10 @@ fn main() {
 
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let decoded_value = decode_bencoded_value(encoded_value)?;
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
+    Ok(())
 }
